7b81982dfb74ccb0bb8da643c1bdf82e2b0e5cd4
Gabriela <aragao.gabi@gmail.com>
2021-09-22 23:11:15 -0300

    Começando o projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace novoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
